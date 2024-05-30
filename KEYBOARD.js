a19a2ad8ee2e55ff58ba59531e7bf41b532004cd
Khush5476 <168684836+Khush5476@users.noreply.github.com>
2024-05-30 17:53:06 -0400

    Add files via upload

---
+++ KEYBOARD.js
@@ -0,0 +1,27 @@
+const display = document.getElementById("display");
+
+const Hidden = document.getElementById("Hidden");
+
+
+
+Hidden.style.visibility = "Hidden"
+hidden.style.visibility = "hidden"
+HiddeN.style.visibility = "hiddeN"
+HIdden.style.visibility = "hIdden"
+HiDden.style.visibility = "hiDden"
+HidDen.style.visibility = "hidDen"
+
+
+
+
+        function appendToDisplay(input){
+            display.value += input;
+        }
+        
+        function Filler(){
+            display.value = "";
+        }
+
+
+
+
